470abe3c33fd05564bf684e11c1f9eb5b9c1546e
Danya D. Smith <danyadsmith@gmail.com>
2016-03-02 09:28:44 -0500

    Complete GulpFile Setup Described in Getting Started with Gulp by Travis Maynard
    This book provides a step-by-step overview of how to set up and use Gulp
    as a task runner for CSS preprocessors, JavaScript minification,
    LiveReload, and other tasks. This file includes integration of:
    
    • beeper - a Node.js package that makes the Terminal beep. This is
    configured to beep when an error occurs.
    • browserify - a Node.js package that bundles JavaScript files and their
    dependencies
    • browser-sync - A Node.js package that provides LiveReload
    functionality while synchronizing the reloads across multiple devices
    connected to the same network. It allows you to live reload code changes on
    desktops, tablets and phones at the same time.
    • gulp-concat - a Gulp package that concatenates files. This is used in
    the Scripts task to concatenate all JavaScript files included in the
    project before minification
    • del - a Node.js package that is used to delete files and folders using
    globs. This is used in the Clean task which can be used to remove all
    processed files from the dist\ directory before starting the task
    runner. A 'reset' option.
    • gulp-imagemin - a Gulp package that is used to minify images. This is
    used in the Images task
    • gulp-jshint - a Gulp package that detects errors and potential problems
    in JavaScript files.
    • gulp-plumber - a Gulp package that manages web asset pipelines. It is
    used in the Styles task.
    • gulp-sass - the Gulp package for my preferred CSS Preprocessor. The book introduces another
    called gulp-myth which I replaced.
    • gulp-sourcemaps - a Gulp package that can be used to create CSS and
    JavaScript source maps which allow you to map code within a compressed
    file back to it’s original position in a source file. This provides an
    easy way to debug minified files which are otherwise unreadable.
    • gulp-uglify - a Gulp package that allows you to use Gulp to minify
    files using UglifyJS.
    • vinyl-source-stream - Gulp uses a virtual file format
    called vinyl to process files. With vinyl-source-stream, we can convert a file stream
    to vinyl for further processing.

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var beeper = require('beeper');
+var browserify = require('browserify');
+var browsersync = require('browser-sync');
+var concat = require('gulp-concat');
+var connect = require('connect');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+function onError(err) {
+  beeper();
+  console.log(err);
+}
+
+gulp.task('browserify', function(){
+  return browserify('./app/js/app.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('browsersync', function(cb){
+  return browsersync({
+    server: {
+      baseDir: './'
+    }
+  }, cb);
+});
+
+gulp.task('clean', function(cb){
+  return del(['dist/*'], cb);
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('app/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('app/css/*.scss')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(sass())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('*.html', browsersync.reload);
+  gulp.watch('app/css/*', 
+    gulp.series('styles', browsersync.reload));
+  gulp.watch('app/js/*.js', 
+    gulp.series('scripts', browsersync.reload));
+  gulp.watch('app/img/*', 
+    gulp.series('images', browsersync.reload));
+});
+
+gulp.task('default', gulp.parallel('browsersync', 'styles', 'scripts', 'images', 'watch'));
